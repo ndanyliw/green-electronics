f51bf90890ca00a32dd34a8a5431967005127e98
Ned Danyliw <ndanyliw@gmail.com>
2015-09-21 09:55:14 -0700

    renamed ge_print to ge_vcom

---
+++ libraries/inc/ge_vcom.h
@@ -0,0 +1,26 @@
+/**
+ * @file  ge_vcom.h
+ * @brief Creates a virtual COM port using the user USB port on
+ * the Discovery board.
+ * 
+ * @author Ned Danyliw
+ * @date  09.2015
+ */
+
+#ifndef _GE_VCOM_H
+#define _GE_VCOM_H
+ 
+#ifdef __cplusplus
+ extern "C" {
+#endif
+
+
+
+
+
+
+#ifdef __cplusplus
+ }
+#endif
+
+ #endif
