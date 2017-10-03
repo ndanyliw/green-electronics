f151e0c8413e4947f116170c2d73529ff1b4014d
Ned Danyliw <ndanyliw@gmail.com>
2017-10-03 18:31:39 -0400

    updated script fix for stm board

--- scripts/stm_rev_d_fix/setup_new_boards.sh
+++ scripts/stm_rev_d_fix/setup_new_boards.sh
@@ -4,3 +4,4 @@
 
 echo "Running script to fix stlink"
 cp stm32f3discovery_revD.cfg ~/ee155_tools/openocd_src/tcl/board/stm32f3discovery_revD.cfg 
+cp stm32f3discovery_revD.cfg ~/ee155_tools/openocd/share/openocd/scripts/board/stm32f3discovery_revD.cfg
